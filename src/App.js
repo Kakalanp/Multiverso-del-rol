2c5a88770353ae98d1c2cb5276f8c106e8437970
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-04-04 21:37:54 -0500

    Adding main component and basic styling to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './styles/App.css';
 import Header from './components/Header';
+import Schedule from './components/Schedule'
 
 function App() {
   return (
+    <>
     <Header />
+    <Schedule />
+    </>
   );
 }
 
---
+++ src/components/Schedule.js
@@ -0,0 +1,7 @@
+import styles from '../styles/Schedule.module.css'
+
+export default function Schedule() {
+    return(
+        <div className={styles.container}></div>
+    )
+}

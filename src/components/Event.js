0680b80840c921fe02b71dca613403f211b3632c
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-04-10 14:19:56 -0500

    Adding Font and basic operations

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,9 +1,50 @@
-export default function Event({master, title, date, time, masterId}) {
-    return(
-        <div>
-            <p>{master}</p>
-            <p>{date}</p>
-            <p>{title}</p>
-        </div>
-    )
+import styles from "../styles/Event.module.css";
+
+export default function Event({ master, title, date, time, masterId }) {
+  // check if there's more than one master
+  if (master.indexOf(", ") !== -1 && master !== "abierto") {
+    master = master.split(", ");
+    masterId = masterId.split(", ");
+    console.log(master, masterId);
+  }
+
+  const localTime = new Date('6/29/2022 4:52:48 PM GMT-1').toLocaleString();
+
+  return (
+    <div className={styles.event}>
+      {
+        //check if links are needed
+        masterId ? (
+          // check if master is an array, meaning is more than one
+          master.constructor === Array ? (
+            //create a link for each master
+            master.map((master, i) => (
+              <a
+                key={masterId[i] * masterId[i]}
+                rel="noopener noreferrer"
+                target="_blank"
+                href={`https://discordapp.com/users/${masterId[i]}`}
+              >
+                {master}
+              </a>
+            ))
+          ) : (
+            // create a single link of there's only one master
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href={`https://discordapp.com/users/${masterId}`}
+            >
+              {master}
+            </a>
+          )
+        ) : (
+          // in case there are no links just add the master
+          <p>{master}</p>
+        )
+      }
+      <p>{localTime}</p>
+      <p>{title}</p>
+    </div>
+  );
 }
